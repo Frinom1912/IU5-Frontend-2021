5f473a8c3cfd86a725366f9a47bc9a0c3c593088
Ivan Bondarenko <ivan.frinom@gmail.com>
2021-03-11 01:10:51 +0300

    done

--- lab3/1.js
+++ lab3/1.js
@@ -6,7 +6,12 @@
  * 'я Вижу солнце' -> 'Я Вижу Солнце'
  */
 function capitalize(str) {
-    // code here
+    let words = str.toLowerCase().split(" ");
+    let result = "";
+    for (let i = 0; i < words.length; i++) {
+        result += (words[i][0].toUpperCase() + words[i].slice(1) + " ");
+    }
+    return result.slice(0, result.length - 1);
 }
 
 module.exports = capitalize;
--- lab3/2.js
+++ lab3/2.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 /**
  * Напишите функцию getMinMax(str),
  * на вход в функцию подается строка str
@@ -8,7 +10,28 @@
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
 function getMinMax(str) {
-    // code here
+    let bez_krinja = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] >= '0' && str[i] <= "9" || str[i] == "-" || str[i] == " " || str[i] == ".")
+            bez_krinja += str[i];
+    }
+    let only_nums = [];
+    bez_krinja = bez_krinja.split(" ");
+    for (let i = 0; i < bez_krinja.length; i++) {
+        if (parseFloat(bez_krinja[i]))
+            only_nums.push(parseFloat(bez_krinja[i]));
+    }
+    let res = {
+        min: only_nums[0],
+        max: only_nums[0]
+    }
+    for (let i = 0; i < only_nums.length; i++) {
+        if (only_nums[i] > res.max)
+            res.max = only_nums[i];
+        if (only_nums[i] < res.min)
+            res.min = only_nums[i];
+    }
+    return res;
 }
 
 module.exports = getMinMax;
